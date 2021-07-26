ca18506ca064fed212f67135e6df94c1fe7a6cdc
Yelamanabi <Yelamanabi@gmail.com>
2021-07-26 10:28:42 -0500

    work 1 is done

--- src/Second.java
+++ src/Second.java
@@ -2,5 +2,6 @@ public class Second {
     public static void main(String[] args) {
         System.out.println("Second hello");
         System.out.println("Hello world");
+        // work is done here
     }
 }
